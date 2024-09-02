a58e357c2930338308aa0c24302ee56e88b421ac
jitae-moon <jitae.moon@icloud.com>
2024-09-02 22:49:24 +0900

    스프링 부트 기본 게시판 개발을 위한 환경설정 추가

---
+++ src/main/java/org/example/springbootbasicboard/SpringBootBasicBoardApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootbasicboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBasicBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootbasicboard/SpringBootBasicBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootbasicboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
